e27b5403eee1387e043afa13697662cf4892c023
ChandanSayed <hangzhou635@gmail.com>
2024-05-03 23:52:02 +0600

    Feature: Global context provider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect } from 'react';
-import { Stack, SplashScreen } from 'expo-router';
-import { useFonts } from 'expo-font';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Stack, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import GlobalProvider from "../context/GlobalProvider";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
-    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
-    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
-    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
-    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
-    'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
-    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
-    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
-    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
-    'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf')
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf")
   });
 
   useEffect(() => {
@@ -34,12 +35,14 @@ const RootLayout = () => {
     return null;
   }
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
   );
 };
 
@@ -48,8 +51,8 @@ export default RootLayout;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   }
 });
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,20 @@
-import { Link, router } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { Image, ScrollView, Text, View } from 'react-native';
-import { images } from '../constants';
-import CustomButton from '../components/CustomButton';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import Logo from '../components/Logo';
+import { Link, Redirect, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Image, ScrollView, Text, View } from "react-native";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Logo from "../components/Logo";
+import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if (isLoading && isLoggedIn) return <Redirect href={"/"} />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="px-4 w-full min-h-full items-center justify-center flex-1">
           <Logo />
           <Image source={images.cards} className="max-w-sm w-full h-80" resizeMode="contain" />
@@ -17,14 +22,21 @@ export default function App() {
             <Text className="text-white text-3xl text-center font-bold">
               Discover Endless Possibilities with <Text className="text-secondary-200">Aora</Text>
             </Text>
-            <Image source={images.path} className="max-w-[136px] w-full h-4 absolute -right-8 -bottom-2" resizeMode="contain" />
+            <Image
+              source={images.path}
+              className="max-w-[136px] w-full h-4 absolute -right-8 -bottom-2"
+              resizeMode="contain"
+            />
           </View>
-          <Text className="font-pRegular text-sm text-gray-100 text-center mt-7">Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora</Text>
+          <Text className="font-pRegular text-sm text-gray-100 text-center mt-7">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with
+            Aora
+          </Text>
           <CustomButton
             title="Continue with Email"
             containerStyle="w-full mt-7"
             handlePress={() => {
-              router.push('/sign-in');
+              router.push("/sign-in");
             }}
           />
         </View>

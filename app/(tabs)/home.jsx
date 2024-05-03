6611a8e87b3c561948931af2ac46bb3abae8e3af
ChandanSayed <hangzhou635@gmail.com>
2024-05-04 01:21:41 +0600

    Feature: Home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
+  const { user } = useGlobalContext();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id: 1 }]}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <Text className="text-3xl text-white">{item.id}</Text>}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pMedium text-sm text-gray-100">Welcome Back</Text>
+                <Text className="text-2xl font-pSemibold text-white capitalize">
+                  {user.username}
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 

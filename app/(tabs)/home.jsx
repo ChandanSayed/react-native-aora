ca2021969a11b2ec1bfbd78566447a3edb52a6ff
ChandanSayed <hangzhou635@gmail.com>
2024-05-05 23:46:13 +0600

    Feature: Homepage

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,7 +1,10 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useGlobalContext } from "../../context/GlobalProvider";
+
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
 
 const Home = () => {
   const { user } = useGlobalContext();
@@ -21,7 +24,11 @@ const Home = () => {
                   {user.username}
                 </Text>
               </View>
+              <View className="mt-1.5">
+                <Image source={images.logoSmall} className="w-9 h-10" resizeMode="contain" />
+              </View>
             </View>
+            <SearchInput />
           </View>
         )}
       />
